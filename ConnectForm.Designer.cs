a0a9cb748670b4d8e3d49e3ed875dd003fdd3991
Rostyslav L <levder@spsejecna.cz>
2023-02-28 19:30:25 +0100

    Add panels and close button.

--- ConnectForm.Designer.cs
+++ ConnectForm.Designer.cs
@@ -31,7 +31,13 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConnectForm));
             this.bgPicture = new System.Windows.Forms.PictureBox();
             this.bgPanel = new System.Windows.Forms.Panel();
+            this.additionalLabel = new System.Windows.Forms.Label();
+            this.welcomePanel = new System.Windows.Forms.Panel();
+            this.dragPanel = new System.Windows.Forms.Panel();
+            this.windowCloseBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.bgPicture)).BeginInit();
+            this.bgPanel.SuspendLayout();
+            this.dragPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // bgPicture
@@ -46,11 +52,61 @@
             // 
             // bgPanel
             // 
-            this.bgPanel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.bgPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bgPanel.Controls.Add(this.additionalLabel);
+            this.bgPanel.Controls.Add(this.welcomePanel);
+            this.bgPanel.Controls.Add(this.dragPanel);
             this.bgPanel.Location = new System.Drawing.Point(405, 0);
             this.bgPanel.Name = "bgPanel";
             this.bgPanel.Size = new System.Drawing.Size(301, 404);
             this.bgPanel.TabIndex = 1;
+            this.bgPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.BgPanel_Paint);
+            // 
+            // additionalLabel
+            // 
+            this.additionalLabel.BackColor = System.Drawing.Color.Transparent;
+            this.additionalLabel.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.additionalLabel.ForeColor = System.Drawing.Color.Gray;
+            this.additionalLabel.Location = new System.Drawing.Point(0, 346);
+            this.additionalLabel.Name = "additionalLabel";
+            this.additionalLabel.Size = new System.Drawing.Size(301, 58);
+            this.additionalLabel.TabIndex = 2;
+            this.additionalLabel.Text = "Free 30-day trial version";
+            this.additionalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // welcomePanel
+            // 
+            this.welcomePanel.BackColor = System.Drawing.Color.Transparent;
+            this.welcomePanel.Location = new System.Drawing.Point(0, 58);
+            this.welcomePanel.Name = "welcomePanel";
+            this.welcomePanel.Size = new System.Drawing.Size(301, 288);
+            this.welcomePanel.TabIndex = 1;
+            // 
+            // dragPanel
+            // 
+            this.dragPanel.BackColor = System.Drawing.Color.Transparent;
+            this.dragPanel.Controls.Add(this.windowCloseBtn);
+            this.dragPanel.Location = new System.Drawing.Point(0, 0);
+            this.dragPanel.Name = "dragPanel";
+            this.dragPanel.Size = new System.Drawing.Size(301, 58);
+            this.dragPanel.TabIndex = 0;
+            this.dragPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DragPanel_MouseDown);
+            this.dragPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DragPanel_MouseMove);
+            // 
+            // windowCloseBtn
+            // 
+            this.windowCloseBtn.BackColor = System.Drawing.Color.Transparent;
+            this.windowCloseBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.windowCloseBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.windowCloseBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Tomato;
+            this.windowCloseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.windowCloseBtn.ForeColor = System.Drawing.Color.White;
+            this.windowCloseBtn.Location = new System.Drawing.Point(255, 12);
+            this.windowCloseBtn.Name = "windowCloseBtn";
+            this.windowCloseBtn.Size = new System.Drawing.Size(34, 34);
+            this.windowCloseBtn.TabIndex = 0;
+            this.windowCloseBtn.Text = "X";
+            this.windowCloseBtn.UseVisualStyleBackColor = true;
             // 
             // ConnectForm
             // 
@@ -63,6 +119,8 @@
             this.Name = "ConnectForm";
             this.Text = "DEFU: Connect";
             ((System.ComponentModel.ISupportInitialize)(this.bgPicture)).EndInit();
+            this.bgPanel.ResumeLayout(false);
+            this.dragPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -71,6 +129,10 @@
 
         private System.Windows.Forms.PictureBox bgPicture;
         private System.Windows.Forms.Panel bgPanel;
+        private System.Windows.Forms.Panel dragPanel;
+        private System.Windows.Forms.Label additionalLabel;
+        private System.Windows.Forms.Panel welcomePanel;
+        private System.Windows.Forms.Button windowCloseBtn;
     }
 }
 
--- ConnectForm.cs
+++ ConnectForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,32 @@ namespace DatabaseEditorForUser
 {
     public partial class ConnectForm : Form
     {
+        Point lastMouseCoor;
+
         public ConnectForm()
         {
             InitializeComponent();
+        }
+
+        private void BgPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle gradient_rectangle = new Rectangle(0, 0, Width, Height);
+            e.Graphics.FillRectangle(new LinearGradientBrush(gradient_rectangle,
+                Color.FromArgb(36, 63, 114), Color.FromArgb(3, 7, 12), 65f), gradient_rectangle);
+        }
+
+        private void DragPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastMouseCoor = e.Location;
+        }
+
+        private void DragPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Left += e.X - lastMouseCoor.X;
+                Top += e.Y - lastMouseCoor.Y;
+            }
         }
     }
 }
