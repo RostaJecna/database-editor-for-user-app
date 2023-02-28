997f79b4da1637412cac7a4d7076d6bd7b684be5
Rostyslav L <levder@spsejecna.cz>
2023-02-28 18:52:25 +0100

    Rename to ConnectForm.cs and add tools.

---
+++ ConnectForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DatabaseEditorForUser
+{
+    partial class ConnectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConnectForm));
+            this.bgPicture = new System.Windows.Forms.PictureBox();
+            this.bgPanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.bgPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bgPicture
+            // 
+            this.bgPicture.Image = ((System.Drawing.Image)(resources.GetObject("bgPicture.Image")));
+            this.bgPicture.Location = new System.Drawing.Point(0, 0);
+            this.bgPicture.Name = "bgPicture";
+            this.bgPicture.Size = new System.Drawing.Size(405, 404);
+            this.bgPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bgPicture.TabIndex = 0;
+            this.bgPicture.TabStop = false;
+            // 
+            // bgPanel
+            // 
+            this.bgPanel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.bgPanel.Location = new System.Drawing.Point(405, 0);
+            this.bgPanel.Name = "bgPanel";
+            this.bgPanel.Size = new System.Drawing.Size(301, 404);
+            this.bgPanel.TabIndex = 1;
+            // 
+            // ConnectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 404);
+            this.Controls.Add(this.bgPanel);
+            this.Controls.Add(this.bgPicture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ConnectForm";
+            this.Text = "DEFU: Connect";
+            ((System.ComponentModel.ISupportInitialize)(this.bgPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bgPicture;
+        private System.Windows.Forms.Panel bgPanel;
+    }
+}
+
--- ConnectForm.cs
+++ ConnectForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace DatabaseEditorForUser
 {
-    public partial class Form1 : Form
+    public partial class ConnectForm : Form
     {
-        public Form1()
+        public ConnectForm()
         {
             InitializeComponent();
         }
