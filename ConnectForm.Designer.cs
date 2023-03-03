24fa9bbdec502de3a560be96dc5da9291dbd0afb
Rostyslav L <levder@spsejecna.cz>
2023-03-03 15:49:00 +0100

    Form design changes.

--- ConnectForm.Designer.cs
+++ ConnectForm.Designer.cs
@@ -54,8 +54,9 @@
             // 
             this.bgPicture.Image = ((System.Drawing.Image)(resources.GetObject("bgPicture.Image")));
             this.bgPicture.Location = new System.Drawing.Point(0, 0);
+            this.bgPicture.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bgPicture.Name = "bgPicture";
-            this.bgPicture.Size = new System.Drawing.Size(405, 404);
+            this.bgPicture.Size = new System.Drawing.Size(540, 497);
             this.bgPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.bgPicture.TabIndex = 0;
             this.bgPicture.TabStop = false;
@@ -65,11 +66,12 @@
             this.bgPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.bgPanel.Controls.Add(this.additionalLabel);
             this.bgPanel.Controls.Add(this.dragPanel);
-            this.bgPanel.Controls.Add(this.connectionPanel);
             this.bgPanel.Controls.Add(this.welcomePanel);
-            this.bgPanel.Location = new System.Drawing.Point(405, 0);
+            this.bgPanel.Controls.Add(this.connectionPanel);
+            this.bgPanel.Location = new System.Drawing.Point(540, 0);
+            this.bgPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bgPanel.Name = "bgPanel";
-            this.bgPanel.Size = new System.Drawing.Size(301, 404);
+            this.bgPanel.Size = new System.Drawing.Size(401, 497);
             this.bgPanel.TabIndex = 1;
             this.bgPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.BgPanel_Paint);
             // 
@@ -78,9 +80,10 @@
             this.additionalLabel.BackColor = System.Drawing.Color.Transparent;
             this.additionalLabel.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.additionalLabel.ForeColor = System.Drawing.Color.Gray;
-            this.additionalLabel.Location = new System.Drawing.Point(0, 346);
+            this.additionalLabel.Location = new System.Drawing.Point(0, 426);
+            this.additionalLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.additionalLabel.Name = "additionalLabel";
-            this.additionalLabel.Size = new System.Drawing.Size(301, 58);
+            this.additionalLabel.Size = new System.Drawing.Size(401, 71);
             this.additionalLabel.TabIndex = 2;
             this.additionalLabel.Text = "Free 30-day trial version";
             this.additionalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -90,8 +93,9 @@
             this.dragPanel.BackColor = System.Drawing.Color.Transparent;
             this.dragPanel.Controls.Add(this.formCloseBtn);
             this.dragPanel.Location = new System.Drawing.Point(0, 0);
+            this.dragPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dragPanel.Name = "dragPanel";
-            this.dragPanel.Size = new System.Drawing.Size(301, 58);
+            this.dragPanel.Size = new System.Drawing.Size(401, 71);
             this.dragPanel.TabIndex = 0;
             this.dragPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DragPanel_MouseDown);
             this.dragPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DragPanel_MouseMove);
@@ -104,9 +108,10 @@
             this.formCloseBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Tomato;
             this.formCloseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.formCloseBtn.ForeColor = System.Drawing.Color.White;
-            this.formCloseBtn.Location = new System.Drawing.Point(255, 12);
+            this.formCloseBtn.Location = new System.Drawing.Point(340, 15);
+            this.formCloseBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.formCloseBtn.Name = "formCloseBtn";
-            this.formCloseBtn.Size = new System.Drawing.Size(34, 34);
+            this.formCloseBtn.Size = new System.Drawing.Size(45, 42);
             this.formCloseBtn.TabIndex = 0;
             this.formCloseBtn.Text = "X";
             this.formCloseBtn.UseVisualStyleBackColor = true;
@@ -116,9 +121,10 @@
             // 
             this.connectionPanel.BackColor = System.Drawing.Color.Transparent;
             this.connectionPanel.Controls.Add(this.connectionStatusLabel);
-            this.connectionPanel.Location = new System.Drawing.Point(0, 58);
+            this.connectionPanel.Location = new System.Drawing.Point(0, 71);
+            this.connectionPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.connectionPanel.Name = "connectionPanel";
-            this.connectionPanel.Size = new System.Drawing.Size(301, 288);
+            this.connectionPanel.Size = new System.Drawing.Size(401, 354);
             this.connectionPanel.TabIndex = 3;
             // 
             // connectionStatusLabel
@@ -126,9 +132,10 @@
             this.connectionStatusLabel.BackColor = System.Drawing.Color.Transparent;
             this.connectionStatusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.connectionStatusLabel.ForeColor = System.Drawing.Color.White;
-            this.connectionStatusLabel.Location = new System.Drawing.Point(41, 75);
+            this.connectionStatusLabel.Location = new System.Drawing.Point(55, 92);
+            this.connectionStatusLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.connectionStatusLabel.Name = "connectionStatusLabel";
-            this.connectionStatusLabel.Size = new System.Drawing.Size(219, 29);
+            this.connectionStatusLabel.Size = new System.Drawing.Size(292, 36);
             this.connectionStatusLabel.TabIndex = 7;
             this.connectionStatusLabel.Text = "Connecting";
             this.connectionStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -140,9 +147,10 @@
             this.welcomePanel.Controls.Add(this.welcomeSubtitleLabel);
             this.welcomePanel.Controls.Add(this.welcomeLineLabel);
             this.welcomePanel.Controls.Add(this.welcomeTitleLabel);
-            this.welcomePanel.Location = new System.Drawing.Point(0, 58);
+            this.welcomePanel.Location = new System.Drawing.Point(0, 71);
+            this.welcomePanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.welcomePanel.Name = "welcomePanel";
-            this.welcomePanel.Size = new System.Drawing.Size(301, 288);
+            this.welcomePanel.Size = new System.Drawing.Size(401, 354);
             this.welcomePanel.TabIndex = 1;
             // 
             // welcomeConnectBtn
@@ -154,10 +162,11 @@
             this.welcomeConnectBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.welcomeConnectBtn.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.welcomeConnectBtn.ForeColor = System.Drawing.Color.White;
-            this.welcomeConnectBtn.Location = new System.Drawing.Point(77, 193);
+            this.welcomeConnectBtn.Location = new System.Drawing.Point(103, 238);
+            this.welcomeConnectBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.welcomeConnectBtn.Name = "welcomeConnectBtn";
-            this.welcomeConnectBtn.Padding = new System.Windows.Forms.Padding(15);
-            this.welcomeConnectBtn.Size = new System.Drawing.Size(147, 82);
+            this.welcomeConnectBtn.Padding = new System.Windows.Forms.Padding(20, 18, 20, 18);
+            this.welcomeConnectBtn.Size = new System.Drawing.Size(196, 101);
             this.welcomeConnectBtn.TabIndex = 9;
             this.welcomeConnectBtn.Text = "Connect To Database";
             this.welcomeConnectBtn.UseVisualStyleBackColor = false;
@@ -168,9 +177,10 @@
             this.welcomeSubtitleLabel.BackColor = System.Drawing.Color.Transparent;
             this.welcomeSubtitleLabel.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.welcomeSubtitleLabel.ForeColor = System.Drawing.Color.LightGray;
-            this.welcomeSubtitleLabel.Location = new System.Drawing.Point(59, 122);
+            this.welcomeSubtitleLabel.Location = new System.Drawing.Point(79, 150);
+            this.welcomeSubtitleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.welcomeSubtitleLabel.Name = "welcomeSubtitleLabel";
-            this.welcomeSubtitleLabel.Size = new System.Drawing.Size(183, 56);
+            this.welcomeSubtitleLabel.Size = new System.Drawing.Size(244, 69);
             this.welcomeSubtitleLabel.TabIndex = 8;
             this.welcomeSubtitleLabel.Text = "Manage your database with a few clicks.\r\n";
             this.welcomeSubtitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -180,9 +190,10 @@
             this.welcomeLineLabel.BackColor = System.Drawing.Color.Transparent;
             this.welcomeLineLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.welcomeLineLabel.ForeColor = System.Drawing.Color.White;
-            this.welcomeLineLabel.Location = new System.Drawing.Point(41, 93);
+            this.welcomeLineLabel.Location = new System.Drawing.Point(55, 114);
+            this.welcomeLineLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.welcomeLineLabel.Name = "welcomeLineLabel";
-            this.welcomeLineLabel.Size = new System.Drawing.Size(219, 29);
+            this.welcomeLineLabel.Size = new System.Drawing.Size(292, 36);
             this.welcomeLineLabel.TabIndex = 7;
             this.welcomeLineLabel.Text = "_________________";
             this.welcomeLineLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -193,9 +204,10 @@
             this.welcomeTitleLabel.Font = new System.Drawing.Font("Sans Serif Collection", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.welcomeTitleLabel.ForeColor = System.Drawing.Color.White;
             this.welcomeTitleLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.welcomeTitleLabel.Location = new System.Drawing.Point(56, 28);
+            this.welcomeTitleLabel.Location = new System.Drawing.Point(75, 29);
+            this.welcomeTitleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.welcomeTitleLabel.Name = "welcomeTitleLabel";
-            this.welcomeTitleLabel.Size = new System.Drawing.Size(189, 65);
+            this.welcomeTitleLabel.Size = new System.Drawing.Size(252, 85);
             this.welcomeTitleLabel.TabIndex = 0;
             this.welcomeTitleLabel.Text = "Database Editor For User";
             this.welcomeTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -207,12 +219,13 @@
             // 
             // ConnectForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(706, 404);
+            this.ClientSize = new System.Drawing.Size(941, 497);
             this.Controls.Add(this.bgPanel);
             this.Controls.Add(this.bgPicture);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ConnectForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DEFU: Connect";
