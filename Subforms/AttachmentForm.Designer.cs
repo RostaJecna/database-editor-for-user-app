c465e4878f7d1b39ad15246e7988543f35fc807d
Rostyslav L <levder@spsejecna.cz>
2023-03-05 19:20:25 +0100

    Upload subforms with basic changes.

---
+++ Subforms/AttachmentForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace DatabaseEditorForUser.Subforms
+{
+    partial class AttachmentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // AttachmentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AttachmentForm";
+            this.Text = "AttachmentForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
