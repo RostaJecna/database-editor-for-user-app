c9092c7608aab7d1fe1d8ace31e96e9e63d4f710
Rostyslav L <levder@spsejecna.cz>
2023-03-15 12:59:21 +0100

    Create a new class that will store the user connection to the database.

---
+++ DatabaseConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseEditorForUser
+{
+    internal class DatabaseConfiguration
+    {
+        public string ServerName { get; set; }
+        public string DatabaseName { get; set; }
+        public bool IntegratedSecurity { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public DatabaseConfiguration(string serverName, string databaseName, string userName, string password)
+        {
+            ServerName = serverName;
+            DatabaseName = databaseName;
+            IntegratedSecurity = false;
+            UserName = userName;
+            Password = password;
+        }
+
+        public DatabaseConfiguration(string serverName, string databaseName)
+        {
+            ServerName = serverName;
+            DatabaseName = databaseName;
+            IntegratedSecurity = true;
+        }
+
+        public override string ToString()
+        {
+            return IntegratedSecurity ? 
+                $"DataSource = {ServerName}, InitialCatalog = {DatabaseName}" : 
+                $"DataSource = {ServerName}, InitialCatalog = {DatabaseName}, IntegratedSecurity = {IntegratedSecurity}" +
+                $"UserID = {UserName}, Password = {Password}";
+        }
+    }
+}
