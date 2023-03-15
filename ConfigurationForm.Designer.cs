0b375648bc5f9ee700d72f726433f4ac7a381e70
Rostyslav L <levder@spsejecna.cz>
2023-03-15 13:02:10 +0100

    Create specifically for the connection control form.

---
+++ ConfigurationForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace DatabaseEditorForUser
+{
+    partial class ConfigurationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfigurationForm));
+            this.serverNameLabel = new System.Windows.Forms.Label();
+            this.serverNameTextBox = new System.Windows.Forms.TextBox();
+            this.authenticationCheckBox = new System.Windows.Forms.CheckBox();
+            this.databaseNameTextBox = new System.Windows.Forms.TextBox();
+            this.databaseNameLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serverNameLabel
+            // 
+            this.serverNameLabel.AutoSize = true;
+            this.serverNameLabel.Location = new System.Drawing.Point(27, 15);
+            this.serverNameLabel.Name = "serverNameLabel";
+            this.serverNameLabel.Size = new System.Drawing.Size(70, 13);
+            this.serverNameLabel.TabIndex = 0;
+            this.serverNameLabel.Text = "Server name:";
+            // 
+            // serverNameTextBox
+            // 
+            this.serverNameTextBox.Location = new System.Drawing.Point(103, 12);
+            this.serverNameTextBox.Name = "serverNameTextBox";
+            this.serverNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.serverNameTextBox.TabIndex = 1;
+            // 
+            // authenticationCheckBox
+            // 
+            this.authenticationCheckBox.AutoSize = true;
+            this.authenticationCheckBox.Location = new System.Drawing.Point(54, 64);
+            this.authenticationCheckBox.Name = "authenticationCheckBox";
+            this.authenticationCheckBox.Size = new System.Drawing.Size(152, 17);
+            this.authenticationCheckBox.TabIndex = 2;
+            this.authenticationCheckBox.Text = "SQL Server Authentication";
+            this.authenticationCheckBox.UseVisualStyleBackColor = true;
+            this.authenticationCheckBox.CheckedChanged += new System.EventHandler(this.IntegratedSecurityCheckBox_CheckedChanged);
+            // 
+            // databaseNameTextBox
+            // 
+            this.databaseNameTextBox.Location = new System.Drawing.Point(103, 38);
+            this.databaseNameTextBox.Name = "databaseNameTextBox";
+            this.databaseNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.databaseNameTextBox.TabIndex = 4;
+            // 
+            // databaseNameLabel
+            // 
+            this.databaseNameLabel.AutoSize = true;
+            this.databaseNameLabel.Location = new System.Drawing.Point(12, 41);
+            this.databaseNameLabel.Name = "databaseNameLabel";
+            this.databaseNameLabel.Size = new System.Drawing.Size(85, 13);
+            this.databaseNameLabel.TabIndex = 3;
+            this.databaseNameLabel.Text = "Database name:";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Enabled = false;
+            this.passwordTextBox.Location = new System.Drawing.Point(103, 109);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.TabIndex = 8;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(41, 112);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(56, 13);
+            this.passwordLabel.TabIndex = 7;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Enabled = false;
+            this.userNameTextBox.Location = new System.Drawing.Point(103, 83);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.userNameTextBox.TabIndex = 6;
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Location = new System.Drawing.Point(36, 86);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(61, 13);
+            this.userNameLabel.TabIndex = 5;
+            this.userNameLabel.Text = "User name:";
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.saveBtn.Location = new System.Drawing.Point(63, 141);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(108, 30);
+            this.saveBtn.TabIndex = 9;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // ConfigurationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 181);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.userNameLabel);
+            this.Controls.Add(this.databaseNameTextBox);
+            this.Controls.Add(this.databaseNameLabel);
+            this.Controls.Add(this.authenticationCheckBox);
+            this.Controls.Add(this.serverNameTextBox);
+            this.Controls.Add(this.serverNameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ConfigurationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Connection configuration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label serverNameLabel;
+        private System.Windows.Forms.TextBox serverNameTextBox;
+        private System.Windows.Forms.CheckBox authenticationCheckBox;
+        private System.Windows.Forms.TextBox databaseNameTextBox;
+        private System.Windows.Forms.Label databaseNameLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.Button saveBtn;
+    }
+}
